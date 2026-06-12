4f064f39f7e2f9e8fe6cac060fb165b4351a6544
AnastasiiaVitiuk <nastavituk730@gmail.com>
2026-06-12 11:59:35 +0100

    Added comments

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -2,17 +2,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+"""
+Script to analyse the data from the NASA EVA dataset. 
+It reads the data from a JSON file, converts it to a CSV file, and then creates a graph of the cumulative time spent in space over the years. 
+The graph is saved as a PNG file and displayed on the screen.
+"""
+
 input_file = open('./eva-data.json', 'r', encoding='ascii')
 output_file = open('./eva-data.csv','w', encoding="utf-8")
 graph_file = 'cumulative_eva_graph.png'
 
 #reading a json file and converting into a dataframe
 eva_df = pd.read_json(input_file, convert_dates=['date'], encoding='ascii')
+#converting eva column to float and dropping na values from the duration and data columns
 eva_df['eva'] = eva_df['eva'].astype(float)
 eva_df.dropna(axis=0, subset=['duration', 'date'], inplace=True)
 
+#writing the file to a csv file
 eva_df.to_csv(output_file, index=False, encoding='utf-8')
 
+#sorting the dataframe by date
 eva_df.sort_values('date', inplace=True)
 
 #convert duration from "HH:MM" format to hours as a float
