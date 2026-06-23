af267fe5401e5e42b0ac7f4afbe919eee90e398e
AnastasiiaVitiuk <nastavituk730@gmail.com>
2026-06-23 12:42:17 +0100

    Added crew size row

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,20 +1,22 @@
 # https://data.nasa.gov/resource/eva.json (with modifications)
-import sys
-
 import matplotlib.pyplot as plt
 import pandas as pd
+import re # added this line
+import sys
 
 """
 Script to analyse the data from the NASA EVA dataset. 
 It reads the data from a JSON file, converts it to a CSV file, and then creates a graph of the cumulative time spent in space over the years. 
 The graph is saved as a PNG file and displayed on the screen.
 """
-
 def main(input_file, output_file, graph_file):
     print("--START--")
 
     # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
@@ -105,6 +107,38 @@ def add_duration_hours(df):
     df_copy = df.copy()
     df_copy["duration_hours"] = df_copy["duration"].apply(
         text_to_duration
+    )
+    return df_copy
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+    
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
     )
     return df_copy
 
